fdd77a54b8273787cb68866f357cffd4f4158d05
陈帅 <qixian.cs@outlook.com>
2018-11-20 20:48:32 +0800

    remove hard code

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -91,7 +91,6 @@ class BasicLayout extends React.PureComponent {
   }
 
   state = {
-    rendering: true,
     isMobile: false,
     menuData: this.getMenuData(),
   };
@@ -103,11 +102,6 @@ class BasicLayout extends React.PureComponent {
     });
     dispatch({
       type: 'setting/getSetting',
-    });
-    this.renderRef = requestAnimationFrame(() => {
-      this.setState({
-        rendering: false,
-      });
     });
     this.enquireHandler = enquireScreen(mobile => {
       const { isMobile } = this.state;
@@ -216,15 +210,14 @@ class BasicLayout extends React.PureComponent {
     });
   };
 
-  renderSettingDrawer() {
+  renderSettingDrawer = () => {
     // Do not render SettingDrawer in production
     // unless it is deployed in preview.pro.ant.design as demo
-    const { rendering } = this.state;
-    if ((rendering || process.env.NODE_ENV === 'production') && APP_TYPE !== 'site') {
+    if (process.env.NODE_ENV === 'production' && APP_TYPE !== 'site') {
       return null;
     }
     return <SettingDrawer />;
-  }
+  };
 
   render() {
     const {
